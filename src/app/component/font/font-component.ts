25cdfd8f3c28bd1c84083e2649f35e961f7ddfee
Genius iQ <soehtetpaing.bless@gmail.com>
2026-02-25 11:26:29 +0630

    font component new integration

--- src/app/component/font/font-component.ts
+++ src/app/component/font/font-component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './font-component.css',
 })
 export class FontComponent {
+  updateFontSize(fontSize: string) {
+    const el = document.querySelectorAll('.toggle-font-size');
+    el.forEach((element: any) => {
+      element.classList.remove('fs-px-12', 'fs-px-14', 'fs-px-16', 'fs-px-18', 'fs-px-20');
+      element.classList.add(fontSize);
+    })
+  }
 
 }
